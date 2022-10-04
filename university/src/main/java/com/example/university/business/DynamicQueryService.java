7463782edbd323951a042dca1cb3c5a008ee25e3
MiellyEllen <maryellen.bowman@gmail.com>
2022-10-04 17:57:33 -0400

    use CourseFilter

--- university/src/main/java/com/example/university/business/DynamicQueryService.java
+++ university/src/main/java/com/example/university/business/DynamicQueryService.java
@@ -1,48 +1,23 @@
 package com.example.university.business;
 
-import com.example.university.dao.CourseDao;
 import com.example.university.domain.Course;
+import com.example.university.repo.CourseRepo;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DynamicQueryService {
 
-    private CourseDao courseDao;
+    private CourseRepo courseRepo;
 
-    private EntityManagerFactory emf;
-
-    private EntityManager em;
-
-    public DynamicQueryService(CourseDao courseDao, EntityManagerFactory emf) {
-        this.courseDao = courseDao;
-        this.em = emf.createEntityManager();
+    public DynamicQueryService(CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
     }
 
-    public List<Course> findCoursesByCriteria(CourseFilter filter) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
-        Root<Course> root = cq.from(Course.class);
-        List<Predicate> predicates = new ArrayList<>();
-        filter.getDepartment().ifPresent(d ->
-                predicates.add(cb.equal(root.get("department"), d)));
-        filter.getCredits().ifPresent(c ->
-                predicates.add(cb.equal(root.get("credits"), c)));
-        filter.getInstructor().ifPresent(i ->
-                predicates.add(cb.equal(root.get("instructor"), i)));
-        cq.select(root).where(predicates.toArray(new Predicate[predicates.size()]));
-        return courseDao.findByCriteria(cq);
-    }
-
-    /*
     public List<Course> filterBySpecification(CourseFilter filter) {
         Specification<Course> courseSpecification =
                 (root, query, criteriaBuilder) -> {
@@ -58,5 +33,5 @@ public class DynamicQueryService {
         return courseRepo.findAll(courseSpecification);
     }
 
-     */
+
 }
--- university/src/main/java/com/example/university/repo/CourseRepo.java
+++ university/src/main/java/com/example/university/repo/CourseRepo.java
@@ -2,11 +2,12 @@ package com.example.university.repo;
 
 import com.example.university.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CourseRepo extends JpaRepository<Course, Integer> {
+public interface CourseRepo extends JpaRepository<Course, Integer>, JpaSpecificationExecutor<Course> {
     Optional<Course> findByName(String name);
     List<Course> findByPrerequisites(Course prerequisite);
     List<Course> findByCredits(int credits);

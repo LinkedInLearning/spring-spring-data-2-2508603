96a6f3ae18115027bb017959da2c2dd7a7a04506
MiellyEllen <maryellen.bowman@gmail.com>
2022-10-08 11:36:11 -0400

    add spec to coursefilter

--- university/src/main/java/com/example/university/business/CourseFilter.java
+++ university/src/main/java/com/example/university/business/CourseFilter.java
@@ -1,8 +1,13 @@
 package com.example.university.business;
 
+import com.example.university.domain.Course;
 import com.example.university.domain.Department;
 import com.example.university.domain.Staff;
+import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -41,5 +46,18 @@ public class CourseFilter {
 
     public Optional<Staff> getInstructor() {
         return instructor;
+    }
+
+    public Specification<Course> getSpecification() {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            department.ifPresent(d ->
+                    predicates.add(criteriaBuilder.equal(root.get("department"), d)));
+            credits.ifPresent(c ->
+                    predicates.add(criteriaBuilder.equal(root.get("credits"), c)));
+            instructor.ifPresent(i ->
+                    predicates.add(criteriaBuilder.equal(root.get("instructor"), i)));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
     }
 }
--- university/src/main/java/com/example/university/business/DynamicQueryService.java
+++ university/src/main/java/com/example/university/business/DynamicQueryService.java
@@ -2,11 +2,8 @@ package com.example.university.business;
 
 import com.example.university.domain.Course;
 import com.example.university.repo.CourseRepo;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,18 +16,7 @@ public class DynamicQueryService {
     }
 
     public List<Course> filterBySpecification(CourseFilter filter) {
-        Specification<Course> courseSpecification =
-                (root, query, criteriaBuilder) -> {
-                    List<Predicate> predicates = new ArrayList<>();
-                    filter.getDepartment().ifPresent(d ->
-                            predicates.add(criteriaBuilder.equal(root.get("department"), d)));
-                    filter.getCredits().ifPresent(c ->
-                            predicates.add(criteriaBuilder.equal(root.get("credits"), c)));
-                    filter.getInstructor().ifPresent(i ->
-                            predicates.add(criteriaBuilder.equal(root.get("instructor"), i)));
-                    return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-                };
-        return courseRepo.findAll(courseSpecification);
+        return courseRepo.findAll(filter.getSpecification());
     }
 
 

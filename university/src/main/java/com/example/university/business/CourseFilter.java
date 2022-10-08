2ec3fed06615742e84827d6f7bceea5ec93d94d3
MiellyEllen <maryellen.bowman@gmail.com>
2022-10-08 16:07:00 -0400

    add queryByExample to coursefilter

--- university/src/main/java/com/example/university/business/CourseFilter.java
+++ university/src/main/java/com/example/university/business/CourseFilter.java
@@ -3,6 +3,7 @@ package com.example.university.business;
 import com.example.university.domain.Course;
 import com.example.university.domain.Department;
 import com.example.university.domain.Staff;
+import com.querydsl.core.BooleanBuilder;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -10,6 +11,8 @@ import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static com.example.university.domain.QCourse.course;
 
 /**
  * Helper class to filter courses in the Dynamic Query Service
@@ -60,6 +63,14 @@ public class CourseFilter {
                     predicates.add(criteriaBuilder.equal(root.get("instructor"), i)));
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
+    }
+
+    public com.querydsl.core.types.Predicate getQueryDslPredicate() {
+        BooleanBuilder predicate = new BooleanBuilder();
+        department.ifPresent(d -> predicate.and(course.department.eq(d)));
+        credits.ifPresent(c -> predicate.and(course.credits.eq(c)));
+        instructor.ifPresent(i -> predicate.and(course.instructor.eq(i)));
+        return predicate;
     }
 
     public Example<Course> getExampleProbe(){
--- university/src/main/java/com/example/university/business/DynamicQueryService.java
+++ university/src/main/java/com/example/university/business/DynamicQueryService.java
@@ -1,9 +1,11 @@
 package com.example.university.business;
 
 import com.example.university.domain.Course;
+import com.example.university.repo.CourseQueryDslRepo;
 import com.example.university.repo.CourseRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -11,17 +13,27 @@ public class DynamicQueryService {
 
     private CourseRepo courseRepo;
 
-    public DynamicQueryService(CourseRepo courseRepo) {
+    private CourseQueryDslRepo queryDslRepo;
+
+    public DynamicQueryService(CourseRepo courseRepo, CourseQueryDslRepo queryDslRepo) {
         this.courseRepo = courseRepo;
+        this.queryDslRepo = queryDslRepo;
     }
 
     public List<Course> filterBySpecification(CourseFilter filter) {
         return courseRepo.findAll(filter.getSpecification());
     }
 
+    public List<Course> findByQueryDsl(CourseFilter filter){
+        List<Course> courses = new ArrayList<>();
+        queryDslRepo
+                .findAll(filter.getQueryDslPredicate())
+                .iterator()
+                .forEachRemaining(courses::add);
+        return courses;
+    }
+
     public List<Course> findByExample(CourseFilter filter){
         return courseRepo.findAll(filter.getExampleProbe());
     }
-
-
 }

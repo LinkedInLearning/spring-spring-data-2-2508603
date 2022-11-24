bb0d4ccca7ab908dc151131b7ee0bad3b5174018
miellyellen <maryellen.bowman@gmail.com>
2022-11-23 21:01:19 -0500

    @Document

--- university/src/main/java/com/example/university/domain/Staff.java
+++ university/src/main/java/com/example/university/domain/Staff.java
@@ -1,13 +1,14 @@
 package com.example.university.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Mongo Document representing a staff member of a department.
  * <p>
  * Created by maryellenbowman
  */
-
+@Document
 public class Staff {
 
     @Id
